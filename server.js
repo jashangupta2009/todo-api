efffc8cb22151dda9f22c1111b9df3a10ffa975b
Jashan <jashangupta2009@gmail.com>
2016-04-26 13:28:43 +0530

    final static todos

--- server.js
+++ server.js
@@ -14,15 +14,23 @@ app.get('/', function(req, res) {
     res.send('Todo rest API');
 });
 
-//getting complete todo list
+//GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
     
+    //filtering for completed query
     if(queryParams.hasOwnProperty("completed") && queryParams.completed === "true") {
         filteredTodos = _.where(filteredTodos, {completed: true});
     } else if(queryParams.hasOwnProperty("completed") && queryParams.completed === "false") {
         filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+    
+    //filtering for some text in description
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+           return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; 
+        });
     }
     
     res.json(filteredTodos);
