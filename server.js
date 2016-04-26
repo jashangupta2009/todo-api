c2a980919e733af84a112ae1e237dcd4dfe96309
Jashan <jashangupta2009@gmail.com>
2016-04-26 11:58:30 +0530

    add put method

--- server.js
+++ server.js
@@ -60,6 +60,34 @@ app.delete('/todos/:id', function(req, res) {
     }
 });
 
+//PUT /todos/:id ...updating individual todo
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var match = _.findWhere(todos, {id: todoId});
+    
+    if(!match) {
+        return res.status(404).send();
+    }
+    
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    
+    _.extend(match, validAttributes);
+    res.json(match);
+});
+
 //listening to the port
 app.listen(PORT, function() {
     console.log('Express listening on port: ' + PORT + "!");
